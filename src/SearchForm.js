4f2827fee5df43d6a47e2a4faeb68adb1870b2c1
MohamedAbdelRazek222 <118555438+MohamedAbdelRazek222@users.noreply.github.com>
2023-02-14 07:24:22 +0200

    Update SearchForm.js

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { useGlobalContext } from './context'
 const SearchForm = () => {
-  //  عشان لو مفيش نتايج رجعت Error,setError و ال  input لألبحث في ال ,setQuery query  هنحط هنا ال 
   const {query,setQuery,error}=useGlobalContext()
-
-
   return <form className="search-form" onSubmit={(e)=>e.preventDefault()}>
     <h2>search movies</h2>
     <input type="text" className="form-input"  value={query} onChange={(e)=>setQuery(e.target.value)}/>
